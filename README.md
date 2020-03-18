9f3f907d8584ec687d8c5e015f20374b4891b11d
Kamil Szczotka <reindonyt@gmail.com>
2020-03-18 14:48:00 +0100

    S.K 2020-03-18

--- README.md
+++ README.md
@@ -12,3 +12,8 @@
 - treść: p.product-review-body
 - wady: div.cons-cell > ul
 - zalety: div.pros-cell > ul
+## Etap 2 - pobranie składowych wszystich opinii z pojedyńczej strony
+-zapisanie składowych opinii w złożonej strukturze danych
+## Etap 3 - pobieranie wszystkich opinii o pojedynczym produkcie 
+-przechodzenie po stronach z opiniami 
+- eksport opinii do pliku (.csv lub .xlsx lub .json)
--- scraper.py
+++ scraper.py
@@ -17,8 +17,26 @@ opinion = opinions.pop()
 opinion_id= opinion["data-entry-id"]
 author=opinion.find("div","reviewer-name-line").string
 recommendation= opinion.find("div","product-review-summary").find("em").string
-stars=  opinion.find("span","review-score-count").string
-purchased = opinion.find("div","product-review-pz").string
-useful =opinion.find("button","vote-yes").find("span").string
-useless =opinion.find("button","vote-no").find("span").string
-content =opinion.find("p","product-review-body").string
+stars = opinion.find("span", "review-score-count").string
+try:
+    purchased = opinion.find("div", "product-review-pz").string
+except AttributeError:
+    purchased = None
+dates = opinion.find("span", "review-time").find_all("time")
+review_date = dates.pop(0)["datetime"]
+try:
+    purchase_date = dates.pop(0)["datetime"]
+except IndexError:
+    purchase_date = None
+useful = opinion.find("button","vote-yes").find("span").string
+useless = opinion.find("button","vote-no").find("span").string
+content = opinion.find("p","product-review-body").get_text()
+try:
+    pros = opinion.find("div", "pros-cell").find("ul").get_text()
+except AttributeError:
+    pros = None
+try:
+    cons = opinion.find("div", "cons-cell").find("ul").get_text()
+except AttributeError:
+    cons = None    
+print(opinion_id, recommendation, stars, content, author, pros, cons, useful, useless, purchased, purchase_date, review_date)
